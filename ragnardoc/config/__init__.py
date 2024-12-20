23346994b377fb53fc8dc388444da6a834443f9e
Gabe Goodhart <ghart@us.ibm.com>
2024-12-20 11:29:27 -0700

    feat: Add global config with user space overrides
    Branch: main
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

---
+++ ragnardoc/config/__init__.py
@@ -0,0 +1,34 @@
+"""
+Central configuration for the tool
+"""
+# Standard
+import os
+
+# First Party
+import aconfig
+
+# Local
+from .merge import merge_configs
+
+
+BASE_CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.yaml")
+
+
+def _initialize_config() -> aconfig.ImmutableConfig:
+    # Parse the base config with env var overrides
+    config = aconfig.Config.from_yaml(BASE_CONFIG_PATH, override_env_vars=True)
+
+    # Merge in user overrides
+    user_config = os.path.join(os.path.expanduser(config.ragnardoc_home), "config.yaml")
+    if os.path.exists(user_config):
+        config = merge_configs(config, user_config)
+
+    # Return the immutable view
+    return aconfig.ImmutableConfig(config)
+
+# Global config object
+_config = _initialize_config()
+
+# Attribute access
+def __getattr__(key: str) -> any:
+    return getattr(_config, key)
