3c753491970e891a30e59e8dda8146c7931a5845
Gabe Goodhart <ghart@us.ibm.com>
2025-01-07 16:31:55 -0700

    feat: Add the "add" command for adding docs and dirs
    Branch: CLISetup
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- ragnardoc/cli/__init__.py
+++ ragnardoc/cli/__init__.py
@@ -3,6 +3,7 @@ Central module for all CLI commands
 """
 
 # Local
+from .add import AddCommand
 from .common import add_common, use_common
 from .init import InitCommand
 from .run import RunCommand
@@ -10,6 +11,7 @@ from .run import RunCommand
 all_commands = {
     cmd.name: cmd
     for cmd in [
+        AddCommand,
         InitCommand,
         RunCommand,
     ]
