4855d993b580bcac1de1de07839cae08d52fa795
Gabe Goodhart <ghart@us.ibm.com>
2025-01-07 15:55:26 -0700

    feat: Add the init command with initial implementation
    There are a lot more smarts that can be added, but this is a decent start.
    We'll also probably want to split up this single module at some point as
    more ingestors get added.
    
    Branch: CLISetup
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- ragnardoc/cli/__init__.py
+++ ragnardoc/cli/__init__.py
@@ -4,11 +4,13 @@ Central module for all CLI commands
 
 # Local
 from .common import add_common, use_common
+from .init import InitCommand
 from .run import RunCommand
 
 all_commands = {
     cmd.name: cmd
     for cmd in [
+        InitCommand,
         RunCommand,
     ]
 }
