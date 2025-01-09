77fbaa126c6ed6f68d102db49483efac9e75b644
Gabe Goodhart <ghart@us.ibm.com>
2025-01-09 10:31:20 -0700

    feat: Initial implementation of OpenWebUI ingestor
    Branch: OpenWebUI
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- ragnardoc/ingestors/__init__.py
+++ ragnardoc/ingestors/__init__.py
@@ -5,6 +5,8 @@ Extensible set of ingestors that can link to multiple RAG consumers
 # Local
 from ..factory import ImportableFactory
 from .anything_llm import AnythingLLMIngestor
+from .open_webui import OpenWebUILLMIngestor
 
 ingestor_factory = ImportableFactory("ingestor")
 ingestor_factory.register(AnythingLLMIngestor)
+ingestor_factory.register(OpenWebUILLMIngestor)
